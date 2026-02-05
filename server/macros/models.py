41bbbb565897b997de693bfca9ad189a2ea031e5
Gwenn01 <arnelgwenn@gmail.com>
2026-02-05 15:28:05 +0800

    fix the macors model

--- server/macros/models.py
+++ server/macros/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from users.models import UserProfile
 from logs.models import  DailyLog
+from django.utils import timezone
 
 class Macros(models.Model):
     user = models.ForeignKey(
@@ -9,11 +10,52 @@ class Macros(models.Model):
         on_delete=models.CASCADE,
         related_name="macros"
     )
-    calories = models.IntegerField()
-    protein = models.IntegerField()
-    carbs = models.IntegerField()
-    fats = models.IntegerField()
+
+    # Core macros
+    calories = models.FloatField(default=0)
+    protein = models.FloatField(default=0)
+    carbs = models.FloatField(default=0)
+    fats = models.FloatField(default=0)
+    fiber = models.FloatField(default=0)
+    sugar = models.FloatField(default=0)
+
+    # Fats breakdown
+    saturated_fat = models.FloatField(default=0)
+    monounsaturated_fat = models.FloatField(default=0)
+    polyunsaturated_fat = models.FloatField(default=0)
+    trans_fat = models.FloatField(default=0)
+
+    # Cholesterol & sodium
+    cholesterol = models.FloatField(default=0)
+    sodium = models.FloatField(default=0)
+
+    # Vitamins
+    vitamin_a = models.FloatField(default=0)
+    vitamin_c = models.FloatField(default=0)
+    vitamin_d = models.FloatField(default=0)
+    vitamin_e = models.FloatField(default=0)
+    vitamin_k = models.FloatField(default=0)
+    vitamin_b1 = models.FloatField(default=0)
+    vitamin_b2 = models.FloatField(default=0)
+    vitamin_b3 = models.FloatField(default=0)
+    vitamin_b6 = models.FloatField(default=0)
+    vitamin_b9 = models.FloatField(default=0)
+    vitamin_b12 = models.FloatField(default=0)
+
+    # Minerals
+    calcium = models.FloatField(default=0)
+    iron = models.FloatField(default=0)
+    magnesium = models.FloatField(default=0)
+    phosphorus = models.FloatField(default=0)
+    potassium = models.FloatField(default=0)
+    zinc = models.FloatField(default=0)
+    copper = models.FloatField(default=0)
+    manganese = models.FloatField(default=0)
+
+    # Day tracking
+    date = models.DateField(default=timezone.now)
+
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.calories
+
+    class Meta:
+        unique_together = ('user', 'date')
