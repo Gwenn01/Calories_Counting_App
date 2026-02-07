82f052abad20c84e9791cad7967b34bdba4183db
Gwenn01 <arnelgwenn@gmail.com>
2026-02-07 21:59:11 +0800

    convert to class base views of foods

--- server/foods/urls.py
+++ server/foods/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('create-food/', views.create_food, name='create_food'),
-    path('get-foods/', views.get_foods, name='get_foods'),
+    path('foods/', views.FoodList.as_view(), name='foods-list'),
+    path('foods/<int:pk>', views.FoodDetail.as_view(), name='food-details'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/foods/views.py
+++ server/foods/views.py
@@ -1,22 +1,48 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Food
 from .serializers import FoodSerializer
 
 # Create your views here.
-@api_view(['POST'])
-def create_food(request):
-    serializer = FoodSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response("Food Inserted Successfully", status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class FoodList(APIView):
+    
+    def get(self, request):
+        foods = Food.objects.all()
+        serializer = FoodSerializer(foods, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = FoodSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Food created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class FoodDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Food.objects.get(pk=pk)
+        except Food.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-# GET ALL FOOD
-@api_view(['GET'])
-def get_foods(request):
-    food = Food.objects.all()
-    serializer = FoodSerializer(food, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, pk):
+        food = self.get_object(pk)
+        serializer = FoodSerializer(food)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        food = self.get_object(pk)
+        serializer = FoodSerializer(food, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Food updated successfully"})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        food = self.get_object(pk)
+        food.delete()
+        return Response({"message": "Food deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
