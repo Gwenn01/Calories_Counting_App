d41d16d392934896293aa91cc8f9c56c11ae2611
Gwenn01 <arnelgwenn@gmail.com>
2026-03-16 23:47:14 +0800

    change app deploy

--- client/api/client.ts
+++ client/api/client.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 import { getToken, removeToken } from "@/utils/token";
 
 export const api = axios.create({
-  baseURL: "https://caloriescountingappserverdeployment.onrender.com/",
+  //baseURL: "https://caloriescountingappserverdeployment.onrender.com/",
   //baseURL: "http://192.168.1.34:8000/",
+  baseURL: "http://192.168.1.38:8000/",
+  //baseURL: "http://127.0.0.1:8000/",
   headers: {
     "Content-Type": "application/json",
   },
