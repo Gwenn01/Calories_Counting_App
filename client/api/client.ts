63ebf2047e67021a89d6c99527fe9503aa215646
Gwenn01 <arnelgwenn@gmail.com>
2026-03-17 13:36:57 +0800

    fix the mobile development

--- client/api/client.ts
+++ client/api/client.ts
@@ -4,8 +4,8 @@ import { getToken, removeToken } from "@/utils/token";
 export const api = axios.create({
   //baseURL: "https://caloriescountingappserverdeployment.onrender.com/",
   //baseURL: "http://192.168.1.34:8000/",
-  baseURL: "http://192.168.1.38:8000/",
-  //baseURL: "http://127.0.0.1:8000/",
+  //baseURL: "http://192.168.1.38:8000/",
+  baseURL: "http://127.0.0.1:8000/",
   headers: {
     "Content-Type": "application/json",
   },
